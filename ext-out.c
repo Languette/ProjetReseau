540046fb8db3b55d71a2879cf2779b862f561184
Languette <34028221+Languette@users.noreply.github.com>
2018-11-20 11:56:37 +0100

    Add files via upload

---
+++ ext-out.c
@@ -0,0 +1,185 @@
+/* echo / serveur simpliste
+   Master Informatique 2012 -- Université Aix-Marseille  
+   Emmanuel Godard
+*/
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/ioctl.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <fcntl.h>
+#include <linux/if.h>
+#include <linux/if_tun.h>
+#include <errno.h>
+
+/* taille maximale des lignes */
+#define MAXLIGNE 60
+#define MAX_BUFF 1500
+#define CIAO "Au revoir ...\n"
+
+ int tunfd;
+int tun_alloc(char *dev){
+  struct ifreq ifr;
+  int fd, err;
+
+  if( (fd = open("/dev/net/tun", O_RDWR)) < 0 ){
+    perror("alloc tun");
+    exit(-1);
+  }
+
+  memset(&ifr, 0, sizeof(ifr));
+
+  /* Flags: IFF_TUN   - TUN device (no Ethernet headers) 
+   *        IFF_TAP   - TAP device  
+   *
+   *        IFF_NO_PI - Do not provide packet information  
+   */ 
+  ifr.ifr_flags = IFF_TUN; 
+  if( *dev )
+    strncpy(ifr.ifr_name, dev, IFNAMSIZ);
+
+  if( (err = ioctl(fd, TUNSETIFF, (void *) &ifr)) < 0 ){
+    close(fd);
+	fprintf(stderr, "\n\nerreur tunalloc\n\n\n");
+    return err;
+  }
+  strcpy(dev, ifr.ifr_name);
+  return fd;
+}
+
+
+void echo(int f, char* hote, char* port);
+
+int main(int argc, char *argv[])
+{
+  int s,n; /* descripteurs de socket */
+  int len,on; /* utilitaires divers */
+  struct addrinfo * resol; /* résolution */
+  struct addrinfo indic = {AI_PASSIVE, /* Toute interface */
+                           PF_INET,SOCK_STREAM,0, /* IP mode connecté */
+                           0,NULL,NULL,NULL};
+  struct sockaddr_in client; /* adresse de socket du client */
+  char * port; /* Port pour le service */
+  int err; /* code d'erreur */
+  
+  /* Traitement des arguments */
+  if (argc!=3) { /* erreur de syntaxe */
+    printf("Usage: %s port interface_tun0\n",argv[0]);
+    exit(1);
+  }
+  
+  port=argv[1]; fprintf(stderr,"Ecoute sur le port %s\n",port);
+  err = getaddrinfo(NULL,port,&indic,&resol); 
+  if (err<0){
+    fprintf(stderr,"Résolution: %s\n",gai_strerror(err));
+    exit(2);
+  }
+  
+  char commands[40];
+  printf("Création de %s\n",argv[2]);
+  tunfd = tun_alloc(argv[2]);
+  sprintf(commands, "ip addr add 172.16.2.1 dev %s", argv[2]);
+  system(commands);
+
+  sprintf(commands, "ip link set dev %s up", argv[2]);
+  system(commands);
+  printf("Interface tun0 Configurée:\n");
+  printf("Appuyez sur une touche pour terminer\n");
+  getchar();
+  
+  
+  /* Création de la socket, de type TCP / IP */
+  if ((s=socket(resol->ai_family,resol->ai_socktype,resol->ai_protocol))<0) {
+    perror("allocation de socket");
+    exit(3);
+  }
+  fprintf(stderr,"le n° de la socket est : %i\n",s);
+
+  /* On rend le port réutilisable rapidement /!\ */
+  on = 1;
+  if (setsockopt(s,SOL_SOCKET,SO_REUSEADDR,&on,sizeof(on))<0) {
+    perror("option socket");
+    exit(4);
+  }
+  fprintf(stderr,"Option(s) OK!\n");
+
+  /* Association de la socket s à l'adresse obtenue par résolution */
+  if (bind(s,resol->ai_addr,sizeof(struct sockaddr_in))<0) {
+    perror("bind");
+    exit(5);
+  }
+  freeaddrinfo(resol); /* /!\ Libération mémoire */
+  fprintf(stderr,"bind!\n");
+
+  /* la socket est prête à recevoir */
+  if (listen(s,SOMAXCONN)<0) {
+    perror("listen");
+    exit(6);
+  }
+  fprintf(stderr,"listen!\n");
+
+  while(1) {
+    /* attendre et gérer indéfiniment les connexions entrantes */
+    len=sizeof(struct sockaddr_in);
+    if( (n=accept(s,(struct sockaddr *)&client,(socklen_t*)&len)) < 0 ) {
+      perror("accept");
+      exit(7);
+    }
+    /* Nom réseau du client */
+    char hotec[NI_MAXHOST];  char portc[NI_MAXSERV];
+    err = getnameinfo((struct sockaddr*)&client,len,hotec,NI_MAXHOST,portc,NI_MAXSERV,0);
+    if (err < 0 ){
+      fprintf(stderr,"résolution client (%i): %s\n",n,gai_strerror(err));
+    }else{ 
+      fprintf(stderr,"accept! (%i) ip=%s port=%s\n",n,hotec,portc);
+    }
+    /* traitement */
+    echo(n,hotec,portc);
+  }
+  return EXIT_SUCCESS;
+}
+
+/* echo des messages reçus (le tout via le descripteur f) */
+void echo(int f, char* hote, char* port)
+{
+  ssize_t lu; /* nb d'octets reçus */
+  char msg[MAXLIGNE+1]; /* tampons pour les communications */ 
+  char tampon[MAX_BUFF];
+  int pid = getpid(); /* pid du processus */
+  int compteur=0;
+
+ 
+  do { /* Faire echo et logguer */
+    lu = recv(f,tampon,MAX_BUFF,0);
+    if (lu > 0 )
+      {  
+		char msg_recv[lu +1];
+		strcpy(msg_recv, "");
+		char partial_msg[1];
+		//strcpy(msg_recv, tampon);
+        compteur++;
+		int i;
+		for(i = 0; i<lu; i++){
+			sprintf(partial_msg, "%c", tampon[i]);
+			strcat(msg_recv, partial_msg);
+		}
+		msg_recv[lu]= '\0';
+		fprintf(stderr,"Taille de message recu : %d\nMessage : \n %s \n",lu, msg_recv);
+        /* log */
+        
+        write(tunfd, msg_recv, strlen(msg_recv));
+      } else {
+        break;
+      }
+  } while ( 1 );
+  /* le correspondant a quitté */
+  send(f,CIAO,strlen(CIAO),0);
+  close(f);
+  fprintf(stderr,"[%s:%s](%i): Terminé.\n",hote,port,pid);
+}
